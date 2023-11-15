c8c657f6aa1cf7d16fa148312c287351c14efeb5
Barry Allahyar <barrya@gmail.com>
2023-11-15 11:32:58 -0600

    test: Create biker_spec with test to check if can initialize with readable attributes

---
+++ spec/biker_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+RSpec.describe Biker do
+  before(:each) do 
+    @biker = Biker.new("Kenny", 30)
+    @biker2 = Biker.new("Athena", 15)
+    @ride1 = Ride.new({name: "Walnut Creek Trail", distance: 10.7, loop: false, terrain: :hills})
+    @ride2 = Ride.new({name: "Town Lake", distance: 14.9, loop: true, terrain: :gravel})
+  end
+
+  describe "#initialize" do
+    it "exists" do
+
+      expect(@biker).to be_a(Biker)
+    end
+
+    it "has readable attributes" do
+      expect(@biker.name).to eq("Kenny")
+      expect(@biker.max_distance).to eq(30)
+      expect(@biker.rides).to eq({})
+      expect(@biker.acceptable_terrain).to eq([])
+    end
+  end
+end
