8bf3763a86ecbf402640cee64d70f096ae9e0023
Barry Allahyar <barrya@gmail.com>
2023-11-15 11:15:25 -0600

    feat: Create #total_distance method

--- lib/ride.rb
+++ lib/ride.rb
@@ -13,4 +13,13 @@ class Ride
   def loop?
     @loop
   end
+
+  #The total distance is double the base distance if the ride is not a loop.
+  def total_distance
+    if @loop == false
+      @distance * 2
+    else
+      @distance
+    end
+  end
 end
