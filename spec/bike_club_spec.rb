158b25bd47793d159e4e0838e0b0368de8f813de
Barry Allahyar <barrya@gmail.com>
2023-11-15 12:55:24 -0600

    test: Create spec and #initialize test for BikeClub Class

---
+++ spec/bike_club_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+RSpec.describe BikeClub do
+  before(:each) do
+    @bike_club = BikeClub.new("Sunset Riders")
+    @biker = Biker.new("Kenny", 30)
+    @biker2 = Biker.new("Athena", 15)
+    @ride1 = Ride.new({name: "Walnut Creek Trail", distance: 10.7, loop: false, terrain: :hills})
+    @ride2 = Ride.new({name: "Town Lake", distance: 14.9, loop: true, terrain: :gravel})
+  end
+
+  describe "#intialize" do
+    it "exists" do
+      expect(@bike_club).to be_a(BikeClub)
+    end
+
+    it "has readable attibutes" do
+      expect(@bike_club.name).to eq("Sunset Riders")
+      expect(@bike_club.bikers).to eq([])
+    end
+  end
+end
