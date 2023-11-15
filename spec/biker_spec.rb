23cfb028bf7f082505cd2397f165779ea171d595
Barry Allahyar <barrya@gmail.com>
2023-11-15 12:27:42 -0600

    test: test to see it can log personal record if ride is completed and requirements are met

--- spec/biker_spec.rb
+++ spec/biker_spec.rb
@@ -67,5 +67,17 @@ RSpec.describe Biker do
       expect(@biker2.personal_record(@ride1)).to eq(false)
       expect(@biker2.personal_record(@ride2)).to eq(false)
     end
+
+    it "can log personal record if ride is completed and requirements are met" do
+      @biker2.learn_terrain!(:gravel)
+      @biker2.learn_terrain!(:hills)
+  
+      @biker2.log_ride(@ride1, 95.0)
+      @biker2.log_ride(@ride2, 65.0)
+  
+      expect(@biker2.rides).to eq({@ride2 => [65.0]})
+      expect(@biker2.personal_record(@ride2)).to eq(65.0)
+      expect(@biker2.personal_record(@ride1)).to eq(false)
+    end
   end
 end
