5aa0eaa92666165d4fa1b6a6de8ef36a43b7ebf8
Barry Allahyar <barrya@gmail.com>
2023-11-15 11:55:03 -0600

    test: Create test for #log_ride method

--- spec/biker_spec.rb
+++ spec/biker_spec.rb
@@ -31,4 +31,18 @@ RSpec.describe Biker do
     end
   end
 
+  describe "#log_ride" do
+    it "can log rides" do
+      @biker.learn_terrain!(:gravel)
+      @biker.learn_terrain!(:hills)
+
+      @biker.log_ride(@ride1, 92.5)
+      @biker.log_ride(@ride1, 91.1)
+      @biker.log_ride(@ride2, 60.9)
+      @biker.log_ride(@ride2, 61.6)
+
+      expect(@biker.rides).to eq({@ride1 => [92.5, 91.1], @ride2 => [60.9, 61.6]})
+    end
+  end
+
 end
