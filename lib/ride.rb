7a928629e6a1972762e0598857fac3505f536def
Barry Allahyar <barrya@gmail.com>
2023-11-15 10:56:46 -0600

    feat: Add #intialize along with readable attributes

--- lib/ride.rb
+++ lib/ride.rb
@@ -1,3 +1,14 @@
 class Ride
+  attr_reader :name,
+              :distance,
+              :terrain
 
+  def initialize(info)
+    @name = info[:name]
+    @distance = info[:distance]
+    @loop = info[:loop]
+    @terrain = info[:terrain]
+  end
+
+  
 end
